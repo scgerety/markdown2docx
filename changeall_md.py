3b6c06e6becc897dbc83bec3010cc4405e7eb527
scgerety <71694412+scgerety@users.noreply.github.com>
2026-03-31 15:33:44 -0400

    added changeall_md.py script.

---
+++ changeall_md.py
@@ -0,0 +1,21 @@
+
+import glob
+import pypandoc
+import os
+
+dir_path = os.path.realpath(os.path.dirname(__file__))
+WORD_DIR = os.path.join(dir_path, "Word Docs")
+
+def convert_md(markdown, stem_name):
+    pypandoc.convert_file(markdown, "docx", outputfile=f"{stem_name}.docx")
+
+def main(directory=dir_path):
+    for filename in glob.glob(f"{directory}\\*.md"):
+        _, f_ext = os.path.split(filename)
+        name, _ = os.path.splitext(f_ext)
+        convert_md(filename, f"{os.path.join(WORD_DIR, name)}")
+
+if __name__ == "__main__":
+    if not os.path.exists(WORD_DIR):
+        os.mkdir(WORD_DIR)
+    main()
